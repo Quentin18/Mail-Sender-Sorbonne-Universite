78d1032e902ba8eda2f233f523e951d26d8469a5
Quentin18 <quentindeschamps18@orange.fr>
2020-04-09 12:13:20 +0200

    Include package data

--- setup.py
+++ setup.py
@@ -5,14 +5,16 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="mailSenderSU",
-    version="0.0.1",
+    version="0.0.3",
     author="Quentin Deschamps",
     author_email="quentindeschamps18@gmail.com",
     description="Mail sender for Sorbonne Universite",
     long_description=long_description,
     long_description_content_type="text/markdown",
     url="https://github.com/Quentin18/Mail-Sender-Sorbonne-Universite",
-    packages=["mailSenderSU"],
+    packages=["mailSenderSU", "mailSenderSU.src",
+              "mailSenderSU.image", "mailSenderSU.data"],
+    include_package_data=True,
     install_requires=['sphinx', 'click'],
     classifiers=[
         "Programming Language :: Python :: 3",
