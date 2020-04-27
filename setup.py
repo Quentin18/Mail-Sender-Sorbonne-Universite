6b73e580e05472e98e1cf6e54e17bce794cb0195
Quentin18 <quentindeschamps18@orange.fr>
2020-04-27 11:36:00 +0200

    Update setup

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="mailSenderSU",
-    version="0.0.7",
+    version="0.0.8",
     author="Quentin Deschamps",
     author_email="quentindeschamps18@gmail.com",
     description="Mail sender for Sorbonne Universite",
